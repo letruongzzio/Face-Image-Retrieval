08511e7c93ae4f73325359cfee59c90c1e9f936a
Đào Xuân Tân <128350618+tandao0909@users.noreply.github.com>
2024-12-20 12:01:55 +0700

    Change to not discard bad images by default. Add concurrency. Due to the inconsistency in the dataset if I drop all the image that the face cannot be unique identified, I decide to keep these images. I also implement concurrency using thread poll.

--- lib/pipeline.py
+++ lib/pipeline.py
@@ -1,16 +1,17 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
 from prepare import download_data
 from processing import crop_face
 import cv2
 
 
-def process_data(remove_images: bool = True):
+def process_data(remove_images: bool = False):
     """
     Download data and process all the image. Now just crop face.
 
     Parameters
     ----------
-    remove_images: bool. Default True
+    remove_images: bool. Default False
         A flag to remove all images that detected no face, or multiple faces
 
     Return
@@ -23,22 +24,32 @@ def process_data(remove_images: bool = True):
     images_path = os.path.join(dataset_path, "img_align_celeba", "img_align_celeba")
     remove_list = []
     print("Begin cropping face")
-    for i, image_filename in enumerate(os.listdir(images_path)):
+
+    def process_image(image_filename):
         image_path = os.path.join(images_path, image_filename)
         image = cv2.imread(image_path)
         state, cropped_image = crop_face(image, 1.12, 9)
-        if i % 20000 == 0:
-            print(f"Done {i // 2000}%")
         if state == "one":
             cv2.imwrite(image_path, cropped_image)
+            return None
         else:
-            remove_list.append(image_path)
+            return image_path
+
+    with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        remove_list = list(executor.map(process_image, os.listdir(images_path)))
+
+    remove_list = [path for path in remove_list if path is not None]
+
     if remove_images:
         print(
             f"Delete {len(remove_list)} images, dues to found none or multiple faces in each of them."
         )
         for remove_path in remove_list:
             os.remove(os.path.join(images_path, remove_path))
+    else:
+        print(
+            f"There are {len(remove_list)} images that can be cropped effectively. They won't be dropped."
+        )
     print("Done prepare dataset. Now you can build embedding query.")
 
 
